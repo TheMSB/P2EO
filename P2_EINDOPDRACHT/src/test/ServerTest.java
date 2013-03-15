35404d25999a8df2b569583da163ef082fad31a7
I3anaan <I3anaan@live.nl>
2013-03-15 13:21:10 +0100

    Vrijwel geheel werkende server, begin JUnit tests

---
+++ P2_EINDOPDRACHT/src/test/ServerTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServerTest {
+
+	@Test
+	public void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testServer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShutDown() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApprove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testJoinLobby() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBroadcastMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFeatures() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConcatArrayList() {
+		fail("Not yet implemented");
+	}
+
+}
