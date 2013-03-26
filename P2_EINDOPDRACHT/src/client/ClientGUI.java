b1adc4fa645940d77f60f0b5e7d4eaa86d9a4728
TheMSB <Martijn.Bruning@gmail.com>
2013-03-26 17:21:39 +0100

    Code Cleanup and GUI restore

--- P2_EINDOPDRACHT/src/client/ClientGUI.java
+++ P2_EINDOPDRACHT/src/client/ClientGUI.java
@@ -3,17 +3,41 @@ package client;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import server.Server;
+
+
 
 public class ClientGUI {
 
+	//CHECKSTYLE:OFF
+	//---- Instance Variables --------------------------
+
+
+	// Windows and Panels
+	private JFrame window = new JFrame("RINGZ");
+	private JPanel menu = new JPanel();
+	private JPanel chatbox = new JPanel();
+	
+	private JPanel board = new JPanel();
+	private JPanel cells[] = new JPanel[25];
+	//CHECKSTYLE:ON
+	
+	//---- Constructor ---------------------------------
+	
+	public ClientGUI(){
+		
+		
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Client client = new Client("Derk"+Math.random());
 		client.start();
-		
+
 	}
 
 }
