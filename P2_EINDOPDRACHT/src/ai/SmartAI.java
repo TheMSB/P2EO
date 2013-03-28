a9b17bc5384a7b4d4378c22828846e6e737b08d0
I3anaan <I3anaan@live.nl>
2013-03-28 14:25:45 +0100

    Begin SmartAI

--- P2_EINDOPDRACHT/src/ai/SmartAI.java
+++ P2_EINDOPDRACHT/src/ai/SmartAI.java
@@ -1,24 +1,37 @@
 package ai;
 
+import game.Board;
+import game.Game;
+import game.Piece;
+import game.Player;
 import game.*;
 
 import java.util.ArrayList;
 
-public class RandomAI implements AI {
-	
+public class SmartAI implements AI {
 	private Game game;
 	private Board board;
 	private Player player;
+	private Integer[][] cells;
 	
 	
-	public RandomAI(Game game, Player player){
+	public SmartAI(Game game, Player player){
 		this.game = game;
 		this.player = player;
 		board = game.getBoard();
+		cells = new Integer[Board.DIM][Board.DIM];
 	}
 	
 	@Override
 	public ArrayList<Integer> getMove() {
+		
+		
+		
+		for(int x=0; x<Board.DIM;x++){
+			for(int y=0; y<Board.DIM;y++){
+				cells[x][y] = calculateWorth(x,y);
+			}
+		}
 		boolean validMoveFound = false;
 		
 		int x;
@@ -43,5 +56,9 @@ public class RandomAI implements AI {
 		arr.add(piece.getColor());
 		return arr;
 	}
-
+	
+	
+	public int calculateWorth(int x, int y){
+		return 0;
+	}
 }
